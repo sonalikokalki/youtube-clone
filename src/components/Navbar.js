9d3397d842bd708824ac05fc933ee86bb289a088
sonalikokalki <sonalikokalki@gmail.com>
2023-08-06 01:00:51 +0530

    Feat: Added Navbar and routes to app

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import {logo} from '../utils/constants'
+
+const Navbar = () => {
+  return (
+    <Stack
+      direction={"row"}
+      alignItems={"center"}
+      p={"2"}
+      sx={{
+        position: "sticky",
+        top: "0",
+        justifyContent: "space-between",
+        backgroundColor: "#000",
+      }}
+    >
+     <Link to='/' style={{display:'flex', alignItems:'center'}}>
+     <img src={logo} alt="logo" height={45}/>
+     </Link>
+    </Stack>
+  );
+};
+
+export default Navbar;
