52623ba88e0d737e97612f1fb17d9f74e82f670c
sonalikokalki <sonalikokalki@gmail.com>
2023-08-11 20:46:56 +0530

    Chore:Added channel card and video card component

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,9 +1,19 @@
+import { Box, Stack } from '@mui/material'
 import React from 'react'
+import VideoCard from './VideoCard'
+import ChannelCard from './ChannelCard'
 
 const Videos = ({videos}) => {
     console.log(videos)
   return (
-    <div>Videos</div>
+    <Stack direction='row' flexWrap='wrap' gap={2} justifyContent='start'>
+      {videos.map((item,id) => (
+        <Box key={id}>
+          {item.id.videoId && <VideoCard video={item}/>}
+          {item.id.channelId && <ChannelCard channel={item}/>}
+        </Box>
+      ))}
+    </Stack>
   )
 }
 
